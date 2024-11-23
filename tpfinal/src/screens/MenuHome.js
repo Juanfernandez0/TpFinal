3502f907ad8b5dd35b571fe61457fb30e6146994
MateoMagarinos <magarinosm@udesa.edu.ar>
2024-11-23 17:57:35 -0300

    arreglo tabnav y comienzo home

--- tpfinal/src/screens/MenuHome.js
+++ tpfinal/src/screens/MenuHome.js
@@ -1,34 +1,53 @@
 
 import react, {Component} from "react";
-import { TouchableOpacity, View, Text, TextInput, StyleSheet  } from "react-native";
+import { TouchableOpacity, View, Text, TextInput, StyleSheet, FlatList  } from "react-native";
 import { db, auth } from "../firebase/config"
-
-
-
-
-
-
+import HomeMenu from "../components/HomeMenu";
 
 
 class MenuHome extends Component{
     constructor() {
         super();
         this.state = {
-            email: '',
-            usuario: '',
-            password: '',
+            posts: [],
+            loading: true
         }
     }
 
-
+componentDidMount(){
+    this.setState({
+        loading: true
+    })
+    db.collection('posts').onSnapshot(
+        docs => {
+            let posts = []
+            docs.forEach(doc => {
+                posts.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+            console.log(posts)
+            this.setState({
+                posts: posts,
+                loading: false
+            })
+        }
+    )
+}
 
 
    
     render(){
         return(
-
+            <View>
+                <Text>Home</Text>
+                <Text>
+                    Info importante
+                </Text>
+                <HomeMenu/>
                 
-                <Text>MenuHome</Text>
+            </View>
 
 
                
