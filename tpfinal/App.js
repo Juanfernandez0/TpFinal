f4f9a8ffe072df3e2db2e996dfca27b2c4dc0e36
Juan Fernandez <juanfernandez@udesa.edu.ar>
2024-11-23 00:40:38 -0300

    Registro y Firebase

---
+++ tpfinal/App.js
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+
+
+
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Register" component={Register}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
